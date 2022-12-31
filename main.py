f88d142c292447f9ce52bc0f04bf22b757998f2d
paul <paul@Debian.lan>
2022-12-31 18:42:24 +0100

    fix black and isort

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from decorator_factory import decorator_factory, DecoratorArgument, DecoratedFunction, DecoratorFolder
+from decorator_factory import (
+    decorator_factory,
+    DecoratorArgument,
+    DecoratedFunction,
+    DecoratorFolder,
+)
 
 from typing import Any
 
 
 @decorator_factory()
 def repeat(
-    func = DecoratedFunction(),
+    func=DecoratedFunction(),
     nb: int = DecoratorArgument(default_value=3, validate_type=True),
-    x: str = DecoratorArgument(default_value='coucou', validate_type=True),
+    x: str = DecoratorArgument(default_value="coucou", validate_type=True),
     **kwargs
 ):
     """
@@ -21,9 +26,9 @@ def repeat(
     return ret
 
 
-@repeat(nb=3, x='toto', test='coucou')
+@repeat(nb=3, x="toto", test="coucou")
 def print_coucou():
-    print('coucou')
+    print("coucou")
 
 
 r = DecoratorFolder()
@@ -32,7 +37,8 @@ r.add(repeat)
 
 @r.repeat
 def print_test():
-    print('test')
+    print("test")
+
 
 if __name__ == "__main__":
     print_coucou()
